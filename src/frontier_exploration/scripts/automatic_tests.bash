6891f7510342b1092a5cc2f96075fda229cbc76b
a.kassmann@hawksracing.de <a.kassmann@hawksracing.de>
2022-09-03 11:17:50 +0200

    add other assigner

--- src/frontier_exploration/scripts/automatic_tests.bash
+++ src/frontier_exploration/scripts/automatic_tests.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # just print this out
-for j in {1..5}
+for j in {1..3}
 do
     for i in {2..6..1}
     do
+        rostest tests run_percentage.test world:=office number_robots:=$i explore_algorithm:=Simple
+	rostest tests run_percentage.test world:=office number_robots:=$i explore_algorithm:=Hungarian
         rostest tests run_percentage.test world:=office number_robots:=$i explore_algorithm:=Auction
     done
 done
